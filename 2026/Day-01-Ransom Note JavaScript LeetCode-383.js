b4e1a2d6dffe1edcce3e65fbefc358dd3c946dfa
Kouser Ahamed <kouserahamed.cse.diu@gmail.com>
2026-07-08 18:42:00 +0600

    LeetCode 383 Ransom Note JavaScript

---
+++ 2026/Day-01-Ransom Note JavaScript LeetCode-383.js
@@ -0,0 +1,24 @@
+// LeetCode 383 Ransom Note JavaScript
+
+var ransomNote = "a";
+var magazine = "b";
+
+var canConstruct = function (ransomNote, magazine) {
+  let map = new Map();
+  for (let i = 0; i < magazine.length; i++) {
+    if (map.has(magazine[i])) {
+      map.set(magazine[i], map.get(magazine[i]) + 1);
+    } else {
+      map.set(magazine[i], 1);
+    }
+  }
+  for (let i = 0; i < ransomNote.length; i++) {
+    if (!map.has(ransomNote[i]) || map.get(ransomNote[i]) === 0) {
+      return false;
+    } else {
+      map.set(ransomNote[i], map.get(ransomNote[i]) - 1);
+    }
+  }
+  return true;
+};
+console.log(canConstruct(ransomNote, magazine)); // Output: false
