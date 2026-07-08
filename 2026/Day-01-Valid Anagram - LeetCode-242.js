77302ae7eab7fe02c47fb0d483b52c3ef2304585
Kouser Ahamed <kouserahamed.cse.diu@gmail.com>
2026-07-08 17:16:38 +0600

    Valid Anagram - LeetCode 242 - JavaScript

---
+++ 2026/Day-01-Valid Anagram - LeetCode-242.js
@@ -0,0 +1,34 @@
+//Valid Anagram - LeetCode 242 - JavaScript
+
+var s = "anagram"
+var t = "nagaram"
+
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    let map = {};
+    for (let i =0 ; i < s.length; i++){
+        let letter = s[i];
+
+        if(!map[letter]){
+            map[letter] = 1;
+        } else {
+            map[letter]++;
+        }
+    }
+
+    for (let i = 0; i < t.length; i++){
+        let letter = t[i];
+        if(map[letter] === undefined){
+
+            return false;
+
+        }
+        if(map[letter]< 1){
+            return false;
+        }
+        map[letter]--;
+    }
+    return true;
+};
+
+console.log(isAnagram(s, t)); // Output: true
